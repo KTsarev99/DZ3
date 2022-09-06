4ea16a0aa792e0286589dc135947798acbcab74c
KTsarev99 <kirilltcarev1999@gmail.com>
2022-09-07 00:30:36 +0300

    задача19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number > 9999 && number < 100000 )
+{
+    int part1 = number% 10;
+    int part2 = number/10000;
+    int part3 = number% 100/10;
+    int part4 = number/1000%10;
+    if(part1 == part2 && part3 == part4)
+    Console.WriteLine("да");
+else
+{
+    Console.WriteLine("нет");
+}
+}
+else
+{
+    Console.WriteLine("Введите пятизначное");
+}
